e3bf481a30679874085be2aa6be7177b1c48da4a
x <me@gmail.com>
2024-10-10 17:55:32 +0800

    errors2 done and errors3 modified

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -16,7 +16,6 @@ fn main() {
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
-
     if cost > tokens {
         println!("You can't afford that many!");
     } else {
