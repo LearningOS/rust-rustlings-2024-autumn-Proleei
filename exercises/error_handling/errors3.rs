155afc35115b8917629d55addcb19c93561438ba
proleei@gmail.com <g1622007622@gmail.com>
2024-10-11 17:42:49 +0800

    error_handling done

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,21 +7,32 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
+    /*
+        这里因为 ? 的行为是遇到 Err 直接返回 Err，众所周知，返回的行为必须在函数上
+        ，所以使用 ? 时，相关的代码必须在一个函数/方法里，而函数也应该返回 
+        Result 类型，如果在 main() 中直接使用则同样需要把 main() 
+        改为 main() -> Result.. 否则编译时会给你这个：
 
-    let cost = total_cost(pretend_user_input)?;
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
-    }
+     */
+    //let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
+    match cost {
+        Ok(cost) => { 
+            if cost > tokens {
+            println!("You can't afford that many!");
+        } else {
+            tokens -= cost;
+            println!("You now have {} tokens.", tokens);
+        }},
+        Err(_) => {println!("NO!!!!");}
+    };
+   
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
