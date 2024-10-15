ebe792e18fe30f53c08bdce9da159051e6cb404a
proleei@gmail.com <g1622007622@gmail.com>
2024-10-15 13:22:51 +0800

    conversions done

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,7 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
@@ -41,6 +40,13 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if (is_valid(tuple.0.clone()) && is_valid(tuple.1.clone()) &&
+         is_valid(tuple.2.clone())) {
+            return Ok(Color{red: tuple.0 as u8,
+                    green: tuple.1 as u8,
+                    blue: tuple.2 as u8});
+         }
+         Err(IntoColorError::IntConversion)
     }
 }
 
@@ -48,6 +54,13 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if (is_valid(arr[0].clone()) && is_valid(arr[1].clone()) &&
+         is_valid(arr[2].clone())) {
+            return Ok(Color{red: arr[0] as u8,
+                    green: arr[1] as u8,
+                    blue: arr[2] as u8});
+         }
+         Err(IntoColorError::IntConversion)
     }
 }
 
@@ -55,7 +68,21 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        if (is_valid(slice[0]) && is_valid(slice[1]) &&
+         is_valid(slice[2])) {
+            return Ok(Color{red: slice[0] as u8,
+                    green: slice[1] as u8,
+                    blue: slice[2] as u8});
+         }
+         Err(IntoColorError::IntConversion)
     }
+}
+
+fn is_valid(value: i16) -> bool{
+    value >= 0 && value <= 255
 }
 
 fn main() {
